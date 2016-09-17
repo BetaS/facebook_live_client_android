bf39d432ef71a31584b28346c5eb875be607845a
mskim <k09089@naver.com>
2016-09-17 17:37:05 +0900

    mskim: project init

---
+++ app/src/main/java/com/fblive/android/library/MainActivity.java
@@ -0,0 +1,23 @@
+package com.fblive.android.library;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+        System.loadLibrary("opencv_java3");
+    }
+}
